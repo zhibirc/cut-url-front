683e758923f235d428491e8ed8b212f475531f04
Yaroslav <y.surilov@infomir.com>
2020-03-21 00:44:41 +0200

    add loader and fix build configs

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,32 @@
 const webpack = require('webpack');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin   = require('vue-loader/lib/plugin');
+
 
 module.exports = {
-
+    entry: './src/main.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new VueLoaderPlugin(),
+    ]
 };

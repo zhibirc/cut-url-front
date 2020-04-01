b1a114622065ea0fb2e48d6c31936e3e2c28bd10
Yaroslav <y.surilov@infomir.com>
2020-04-02 00:23:15 +0300

    refactor

---
+++ tests/api.spec.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+const assert = require('chai').assert;
+
+const api = require('../src/api');
+
+describe('', () => {
+    it('', () => {});
+});
--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -2,6 +2,12 @@
 
 const assert = require('chai').assert;
 
+describe('Initial support', () => {
+    it("should be able to execute a test", () => {
+        assert.equal(true, true);
+    });
+});
+
 describe('Check exports correctness', () => {
     it('"main" module', () => {
         const main = require('../src/main');
